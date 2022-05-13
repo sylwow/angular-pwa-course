da73ce43d3cb7660e413e761055d302fbd96edad
Sylwek mac <Sylwo1003@gmail.com>
2022-05-13 13:28:26 +0200

    extend manual service worker

--- src/sw.js
+++ src/sw.js
@@ -1,44 +1,63 @@
-
-const VERSION = 'v9';
-
+const VERSION = 'v13';
 
 log('Installing Service Worker');
 
-
 self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
-
-/*
-
-    These are the files that we want to download and install on the background
-
-        '/',
-        '/polyfills.bundle.js',
-        '/inline.bundle.js',
-        '/styles.bundle.js',
-        '/vendor.bundle.js',
-        '/main.bundle.js',
-        '/assets/bundle.css',
-        '/assets/angular-pwa-course.png',
-        '/assets/main-page-logo-small-hat.png'
-*/
 
 async function installServiceWorker() {
 
     log("Service Worker installation started ");
 
+    const cache = await caches.open(getCacheName());
+
+    return cache.addAll([
+        '/',
+        '/polyfills.js',
+        '/styles.js',
+        '/vendor.js',
+        '/main.js',
+        '/api/lessons',
+        '/assets/bundle.css',
+        '/assets/angular-pwa-course.png',
+        '/assets/main-page-logo-small-hat.png'
+    ]);
 }
 
-self.addEventListener('activate', () => {
+function getCacheName() {
+    return 'app-cache-' + VERSION;
+}
 
+self.addEventListener('activate', () => astivateSw());
+
+self.addEventListener('fetch', event => event.respondWith(cacheThenNetwork(event)));
+
+async function astivateSw() {
     log('Service Worker activated');
-});
 
+    const cacheKeys = await caches.keys();
 
+    cacheKeys.forEach(cacheKey => {
+        if (cacheKey !== getCacheName()) {
+            caches.delete(cacheKey);
+        }
+    });
+}
 
+async function cacheThenNetwork(event) {
+    const cache = await caches.open(getCacheName());
 
+    const cachedResponse = await cache.match(event.request);
 
+    if (cachedResponse) {
+        log('from cache: ' + event.request.url)
+        return cachedResponse;
+    } else {
+        const networkResponse = await fetch(event.request);
+        log('from network: ' + event.request.url);
 
-
+        return networkResponse;
+    }
+}
 
 function log(message, ...data) {
     if (data.length > 0) {
